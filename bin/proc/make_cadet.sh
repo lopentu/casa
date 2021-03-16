6947a8c062ae45c061931c8eb381b83cadccda7b
seantyh <seantyh@gmail.com>
2021-03-16 13:07:55 +0800

    add make_cadet.sh

---
+++ bin/proc/make_cadet.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "missing argument: input_csvs"
+    echo "USAGE: make_cadet.sh <input_dir>"
+    exit;
+fi;
+
+CADET_PREFIX=alpha
+PYTHONBIN=python
+echo "Making cadet model"
+echo "Input dir: $1"
+echo "cadet prefix: ${CADET_PREFIX}"
+
+INPUT_DIR=$1
+# csv -> pickles
+echo "Converting csv to python pickles"
+
+for csv in $INPUT_DIR/*.csv; do          
+    pkl_path=${csv/.csv/.pkl}        
+    if [ ! -f $pkl_path ]; then
+        $PYTHONBIN make_threads.py $csv
+    else
+        echo "$pkl_path already exists, skipped"
+    fi;
+done;
+
+# merge pickles
+pkl_files="$INPUT_DIR/*.pkl"
+merged_file="$INPUT_DIR/threads_merged.pkl"
+if [ ! -f $merged_file ]; then
+    $PYTHONBIN merge_threads.py "$pkl_files" $merged_file
+else
+    echo "threads already merged, skipped"
+fi;
+
+# building model
+
+echo "Building cadet model"
+$PYTHONBIN build_cadet.py "$merged_file" --prefix cadet/$CADET_PREFIX
+
+echo "NOTE: cadet model is built under <CASA_ROOT>/data/cadet/$CADET_PREFIX."
+echo "      Cadet still need the seed table, seed.csv."
+echo "      Please place the appropriate copy into cadet/$CADET_PREFIX"
--- src/casa/thread_formatter.py
+++ src/casa/thread_formatter.py
@@ -53,3 +53,38 @@ def format_thread_html(thread, idx=-1):
     sio.write("</ol>\n")
     sio.write("</div>")
     return sio.getvalue()
+
+def format_thread_by_type_html(thread, idx=-1, optype="main"):
+    sio = StringIO()
+    op0 = thread.get_opinion()
+
+    sio.write(f"<div class='thread-part-wrapper' id='{op0.id}-{optype}'>\n")
+    if optype == "main":
+        if not op0.id:
+            timestamp = ""
+        else:
+            timestamp = datetime.strptime(op0.id[1:15], 
+                                    "%Y%m%d%H%M%S").strftime("%c")    
+        sio.write(f"<span class='source'>{idx}. [{thread.source}] </span>")
+        sio.write("<span class='timestamp'>" + timestamp + "</span>\n")
+        if thread.main:
+            sio.write("<div class='title'>")
+            sio.write(format_text(thread.main.title, __indent=0))
+            sio.write("</div>\n<div class='main-text'>\n")
+            sio.write(format_text(thread.main.text, __indent=0))
+            sio.write("</div>\n\n")
+        else:
+            sio.write("<div class='title'>(無主文)</div>")
+    
+    elif optype == "reply":        
+        sio.write("<ol class='reply-wrap'>\n")
+        for reply_idx, reply_x in enumerate(thread.replies):
+            sio.write(format_text(f"  <li class='reply-text' id='reply-{reply_idx}'> " 
+                + reply_x.text, __indent=3, __indent0=0))
+            sio.write("  </li>\n")
+        sio.write("</ol>\n")
+    
+    else:
+        raise ValueError("not supported opType: " + optype)
+    sio.write("</div>")
+    return sio.getvalue()
