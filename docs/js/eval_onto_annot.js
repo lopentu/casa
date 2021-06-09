41d43d39007f07b46ee8ebf4030d5c58cac8b492
seantyh <seantyh@gmail.com>
2021-06-10 00:42:18 +0800

    add eval-ontology page

--- docs/js/eval_onto_annot.js
+++ docs/js/eval_onto_annot.js
@@ -4,20 +4,22 @@ var app = new Vue({
     el: '#app',
     created: function(){
         console.log("heelo");
-        fetch('data/eval_ontology_raw_210605.json')
-            .then(resp=>resp.json())
-            .then(data=>{
+        fetch('data/eval_ontology_raw_210605.json')            
+            .then(resp=>resp.text())
+            .then(text=>{
+                text = text.replace(/NaN/g, "null");
+
+                data = JSON.parse(text);
                 console.log(data);
                 this.onto = data;
             });
     },
     
-    data: {
-      message: 'Hello Vue!',
+    data: {      
       onto: {},      
       show_ngrams: true,
       show_words: false,
-      show_evals: false
+      show_evals: true
     },
 
     methods: {
